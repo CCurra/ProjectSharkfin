c5783e6d138545e77eebfdbd44e0adcb917802e7
Memoore1988 <Devilishtroy@gmail.com>
2019-05-16 20:27:26 -0500

    Moore - Setup Files added for packaging

---
+++ Source/Sharkfin/Cplus_Placeholder.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Cplus_Placeholder.h"
+
+// Sets default values
+ACplus_Placeholder::ACplus_Placeholder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACplus_Placeholder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACplus_Placeholder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
